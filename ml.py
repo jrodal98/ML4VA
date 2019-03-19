463f33924f6ad1b0f5ada85b39dfb5ea8885d410
Jake Rodal <jrodal98@gmail.com>
2019-03-19 18:29:53 -0400

    created 8 models

--- ml.py
+++ ml.py
@@ -1,4 +1,16 @@
 # %%
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+import time
+import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 import warnings
 from sklearn.metrics import roc_auc_score
@@ -16,6 +28,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from config import api_key
+from gmplot import gmplot
 %matplotlib inline
 
 # %%
@@ -41,10 +55,109 @@ def fix_time(time):
 df["DateIssued"] = df["DateIssued"].apply(lambda x: x[:10] if int(x[:2]) <= 21 else np.nan)
 df["TimeIssued"] = df["TimeIssued"].apply(fix_time)
 df.head()
+
 # %%
-X = df.iloc[:, [0, 1, 2, 3, 5, 6]]
+df = df.where((df["latitude"] < 38.4) & (df["AppealStatus"] != "pending"))
+df.dropna(inplace=True)
+
+# %%
+gmap = gmplot.GoogleMapPlotter(df["latitude"].mean(), df["longitude"].mean(), 14)
+gmap.apikey = api_key
+# gmap.scatter(df["latitude"], df["longitude"], '#FF0000', size=10, marker=False)
+gmap.heatmap(df["latitude"], df["longitude"])
+gmap.draw("my_map.html")
+
+# %%
+# X = df.iloc[:, [1, 2, 3, 5, 6]]
+X = df.iloc[:, [3, 5, 6]]  # don't look at date or time right now (interesting strategy, I know).
 y = df.iloc[:, 4]
+new_vio = LabelEncoder().fit(X["ViolationDescription"]).transform(X["ViolationDescription"])
+new_appeal = LabelEncoder().fit(y).transform(y)
+X["ViolationDescription"] = new_vio
+y = new_appeal
+X = StandardScaler().fit(X).transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
-df_train = pd.concat([X_train, y_train], axis=1)
-df_test = pd.concat([X_test, y_test], axis=1)
+
+
 # %%
+dict_classifiers = {
+    "Logistic Regression": LogisticRegression(),
+    "Nearest Neighbors": KNeighborsClassifier(),
+    "Linear SVM": SVC(),
+    "Gradient Boosting Classifier": GradientBoostingClassifier(n_estimators=1000),
+    "Decision Tree": tree.DecisionTreeClassifier(),
+    "Random Forest": RandomForestClassifier(n_estimators=1000),
+    "Neural Net": MLPClassifier(alpha=1),
+    "Naive Bayes": GaussianNB()
+}
+# %%
+
+
+def batch_classify(X_train, Y_train, X_test, Y_test, no_classifiers=5, verbose=True):
+    """
+    This method, takes as input the X, Y matrices of the Train and Test set.
+    And fits them on all of the Classifiers specified in the dict_classifier.
+    The trained models, and accuracies are saved in a dictionary. The reason to use a dictionary
+    is because it is very easy to save the whole dictionary with the pickle module.
+
+    Usually, the SVM, Random Forest and Gradient Boosting Classifier take quiet some time to train.
+    So it is best to train them on a smaller dataset first and
+    decide whether you want to comment them out or not based on the test accuracy score.
+    """
+
+    dict_models = {}
+    for classifier_name, classifier in list(dict_classifiers.items())[:no_classifiers]:
+        t_start = time.clock()
+        classifier.fit(X_train, Y_train)
+        t_end = time.clock()
+
+        t_diff = t_end - t_start
+        train_score = classifier.score(X_train, Y_train)
+        test_score = classifier.score(X_test, Y_test)
+
+        dict_models[classifier_name] = {
+            'model': classifier, 'train_score': train_score, 'test_score': test_score, 'train_time': t_diff}
+        if verbose:
+            print("trained {c} in {f:.2f} s".format(c=classifier_name, f=t_diff))
+    return dict_models
+
+
+def display_dict_models(dict_models, sort_by='test_score'):
+    cls = [key for key in dict_models.keys()]
+    test_s = [dict_models[key]['test_score'] for key in cls]
+    training_s = [dict_models[key]['train_score'] for key in cls]
+    training_t = [dict_models[key]['train_time'] for key in cls]
+
+    df_ = pd.DataFrame(data=np.zeros(shape=(len(cls), 4)), columns=[
+                       'classifier', 'train_score', 'test_score', 'train_time'])
+    for ii in range(0, len(cls)):
+        df_.loc[ii, 'classifier'] = cls[ii]
+        df_.loc[ii, 'train_score'] = training_s[ii]
+        df_.loc[ii, 'test_score'] = test_s[ii]
+        df_.loc[ii, 'train_time'] = training_t[ii]
+
+    display(df_.sort_values(by=sort_by, ascending=False))
+
+
+# %%
+dict_models = batch_classify(X_train, y_train, X_test, y_test, no_classifiers=8)
+display_dict_models(dict_models)
+
+# %%
+svm_clf = LinearSVC(C=1, loss="hinge", random_state=42, max_iter=1000)
+svm_clf.fit(X_train, y_train)
+
+
+def test_svm(svm, X_test, y_test):
+    predictions = svm.predict(X_test)
+    acc = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions)
+    recall = recall_score(y_test, predictions)
+    f1 = f1_score(y_test, predictions)
+    print(f"Accuracy:{acc}\nPrecision:{precision}\nRecall:{recall}\nF1 Score:{f1}")
+
+
+test_svm(svm_clf, X_test, y_test)
+
+# %%
+# awesome code for modeling http://ataspinar.com/2017/05/26/classification-with-scikit-learn/
